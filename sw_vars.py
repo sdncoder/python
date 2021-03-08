05b4df3761ae0427a007eb5c0f7ab598338512f0
the network stuff <16766408+sdncoder@users.noreply.github.com>
2021-03-08 12:03:55 -0500

    Create sw_vars.py

---
+++ sw_vars.py
@@ -0,0 +1,9 @@
+# variable file for switch config
+hostname = 'USJ-IDF-228'
+loopback1 = '10.236.0.10'
+loopback2 = '10.236.128.10'
+loopback3 = '10.237.0.10'
+loopback4 = '10.237.128.10'
+loopback5 = '10.238.0.10'
+loopback6 = '10.238.128.10'
+vlan10 = '10.236.96.1 255.255.255.0'
