1464e3e2e5939905eb033469db048116a06c93c5
the network stuff <16766408+sdncoder@users.noreply.github.com>
2021-04-01 17:16:36 -0400

    Update cdr2mask.py

--- cdr2mask.py
+++ cdr2mask.py
@@ -1,11 +1,4 @@
 
-I have a list containing hundreds of ip addresses and masks in this format:
-
-['10.10.10.0/255.255.255.0', '10.10.20.0/255.255.255.192']
-I would like to convert this list to this format:
-
-['10.10.10.0/24', '10.10.20.0/26']
-Here is my code:
 
 # List already containing all ip/mask (format: 10.10.10.0/255.255.255.0') shown above
 print ip_mask_list
